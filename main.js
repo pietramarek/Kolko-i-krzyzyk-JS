c3229806136733ec8a5fe27294f709b4a00370c1
pietramarek <pietramarek@gmail.com>
2023-04-14 22:43:12 +0200

    Poprawiony mechanizm działania kodu

--- main.js
+++ main.js
@@ -1,168 +1,94 @@
-const winingArray1 = [];
-const winingArray2 = [];
-//reset strony
+let winingArray1 = [];
+let winingArray2 = [];
+//reset strony------------------
 const reset = document.querySelector("#reset");
 reset.addEventListener("click", () => {
   window.location.reload(true);
 });
+//---------------------------------------
 
 let figures = document.querySelector(".figure");
 let click = document.querySelectorAll(".figure");
-
+let input = document.querySelector(".input");
 let i = 0;
-let znak = "X";
+
+//------------------------------------------------
 
 click.forEach((figures, index) => {
   figures.addEventListener("click", () => {
-    figures.append(znak);
-    i++;
-    if (i === 1 || i === 3 || i === 5 || i === 7) {
-      znak = "O";
-      winingArray1.push(index);
-    } else {
-      znak = "X";
-      winingArray2.push(index);
+    console.log(i);
 
+    if (i === 0 || i === 2 || i === 4 || i === 6 || i === 8) {
+      var znak = "O";
+      figures.append(znak);
+      winingArray1.push(index);
       console.log(winingArray1);
+      console.log(winingArray2);
+      i++;
       console.log(i);
+    } else if (i === 1 || i === 3 || i === 5 || i === 7) {
+      var znak = "X";
+      figures.append(znak);
+      winingArray2.push(index);
+      i++;
+      console.log(winingArray1);
       console.log(winingArray2);
     }
+
+    if (
+      (winingArray1.includes(0) &&
+        winingArray1.includes(1) &&
+        winingArray1.includes(2)) ||
+      (winingArray1.includes(3) &&
+        winingArray1.includes(4) &&
+        winingArray1.includes(5)) ||
+      (winingArray1.includes(6) &&
+        winingArray1.includes(7) &&
+        winingArray1.includes(8)) ||
+      (winingArray1.includes(0) &&
+        winingArray1.includes(3) &&
+        winingArray1.includes(6)) ||
+      (winingArray1.includes(1) &&
+        winingArray1.includes(4) &&
+        winingArray1.includes(7)) ||
+      (winingArray1.includes(2) &&
+        winingArray1.includes(5) &&
+        winingArray1.includes(8)) ||
+      (winingArray1.includes(0) &&
+        winingArray1.includes(4) &&
+        winingArray1.includes(8)) ||
+      (winingArray1.includes(2) &&
+        winingArray1.includes(4) &&
+        winingArray1.includes(6))
+    ) {
+      input.append("Wygrałeś O");
+    } else if (
+      (winingArray2.includes(0) &&
+        winingArray2.includes(1) &&
+        winingArray2.includes(2)) ||
+      (winingArray2.includes(3) &&
+        winingArray2.includes(4) &&
+        winingArray2.includes(5)) ||
+      (winingArray2.includes(6) &&
+        winingArray2.includes(7) &&
+        winingArray2.includes(8)) ||
+      (winingArray2.includes(0) &&
+        winingArray2.includes(3) &&
+        winingArray2.includes(6)) ||
+      (winingArray2.includes(1) &&
+        winingArray2.includes(4) &&
+        winingArray2.includes(7)) ||
+      (winingArray2.includes(2) &&
+        winingArray2.includes(5) &&
+        winingArray2.includes(8)) ||
+      (winingArray2.includes(0) &&
+        winingArray2.includes(4) &&
+        winingArray2.includes(8)) ||
+      (winingArray2.includes(2) &&
+        winingArray2.includes(4) &&
+        winingArray2.includes(6))
+    ) {
+      input.append("Wygrałeś X");
+    }
   });
-});
-
-figures.addEventListener("click", () => {
-  if (
-    winingArray1.includes(0) &&
-    winingArray1.includes(1) &&
-    winingArray1.includes(2)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś X");
-  } else if (
-    winingArray1.includes(3) &&
-    winingArray1.includes(4) &&
-    winingArray1.includes(5)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś X");
-  } else if (
-    winingArray1.includes(6) &&
-    winingArray1.includes(7) &&
-    winingArray1.includes(8)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś X");
-  } else if (
-    winingArray1.includes(0) &&
-    winingArray1.includes(3) &&
-    winingArray1.includes(6)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś X");
-  } else if (
-    winingArray1.includes(1) &&
-    winingArray1.includes(4) &&
-    winingArray1.includes(7)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś X");
-  } else if (
-    winingArray1.includes(2) &&
-    winingArray1.includes(5) &&
-    winingArray1.includes(8)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś X");
-  } else if (
-    winingArray1.includes(0) &&
-    winingArray1.includes(4) &&
-    winingArray1.includes(8)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś X");
-  } else if (
-    winingArray1.includes(2) &&
-    winingArray1.includes(4) &&
-    winingArray1.includes(6)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś X");
-  }
-});
-
-document.addEventListener("click", () => {
-  if (
-    winingArray2.includes(0) &&
-    winingArray2.includes(1) &&
-    winingArray2.includes(2)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś O");
-    return;
-  } else if (
-    winingArray2.includes(3) &&
-    winingArray2.includes(4) &&
-    winingArray2.includes(5)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś O");
-  } else if (
-    winingArray2.includes(6) &&
-    winingArray2.includes(7) &&
-    winingArray2.includes(8)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś O");
-  } else if (
-    winingArray2.includes(0) &&
-    winingArray2.includes(3) &&
-    winingArray2.includes(6)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś O");
-  } else if (
-    winingArray2.includes(1) &&
-    winingArray2.includes(4) &&
-    winingArray2.includes(7)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś O");
-  } else if (
-    winingArray2.includes(2) &&
-    winingArray2.includes(5) &&
-    winingArray2.includes(8)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś O");
-  } else if (
-    winingArray2.includes(0) &&
-    winingArray2.includes(4) &&
-    winingArray2.includes(8)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś O");
-  } else if (
-    winingArray2.includes(2) &&
-    winingArray2.includes(4) &&
-    winingArray2.includes(6)
-  ) {
-    win = 1;
-    let input = document.querySelector(".input");
-    input.append("Wygrałeś O");
-  }
 });
